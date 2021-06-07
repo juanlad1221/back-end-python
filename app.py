6fdd55fe2d475cc2a86a9612e66fba38c8f61284
Juan Campos <juanlad1221@gmail.com>
2021-06-07 00:58:50 -0300

    api
    first commit and creation of api rest

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import request
+from data import data as data
+import json
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET', 'PUT'])
+def apiGet():
+    if request.method == 'GET':
+        return json.dumps(data)
+
+    if request.method == 'PUT':
+        data_to_update = request.form['data']
+
+        return str(data_to_update)
+
+
+    
+        
+        
+
+
+if __name__ =='__main__':
+    app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,194 @@
+import random
+
+data = [
+    {
+        'id': 1,
+        'paciente': 'juan ponce',
+        'odontologo': 'Raul Castro',
+        'placas': 2,
+        'inicio': '21/08/21',
+        'fin': '22/09/21'
+    },
+
+    {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'lorena Sierra',
+        'odontologo': 'Raul Castro',
+        'placas': 0,
+        'inicio': '11/05/21',
+        'fin': '23/06/21'
+    },
+
+    {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    },
+    {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    },
+    {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    },
+    {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    },
+    {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    },
+    {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    },
+    {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    },
+    {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    },
+    {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    }, {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    },
+    {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    }, {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    }, {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    }, {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    }, {
+       ' id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    }, {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    }, {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    }, {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    }, {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    }, {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    }, {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    }, {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    }, {
+        'id': str(random.getrandbits(8)),
+        'paciente': 'juana Molina',
+        'odontologo': 'Jorge Cardozo',
+        'placas': 1,
+        'inicio': '08/08/21',
+        'fin': '09/08/21'
+    }
+]
+
