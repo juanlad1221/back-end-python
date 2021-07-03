c9e7b1ee60f8f627ff202c094f7f49b11aa50ced
Juan Campos <juanlad1221@gmail.com>
2021-07-03 12:39:06 -0300

    se

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
 from flask import Flask, jsonify
-from flask import request
+from flask import request, Response
+from flask_pymongo import PyMongo
+from bson import json_util
 
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = "mongodb+srv://juanlad1221:yonome1221@clustershool-widaw.mongodb.net/db_app_profes_iar?retryWrites=true"
+mongo = PyMongo(app)
+
 
 @app.route('/', methods=['GET'])
 def api():
-    if request.method == 'GET':
-        return jsonify([{'name':'Matematicas'},{'name':'Ingles'}])
+    if request.method  == 'GET':
+        materias = mongo.db.materias.find({})
+        result = json_util.dumps(materias)
+        return Response(result, mimetype='application/json')
 
     
 
 
-    
-        
+
+
+
+
+#Not found
+@app.errorhandler(404)
+def not_found(error = None):
+    response = jsonify({
+        'message':'Recourse not found: ' + request.url,
+        'status':404
+    }) 
+    response.status_code = 404  
+    return response 
         
 
 
